07e665d868cc76320e142e5b6f8f714a3c75a26a
YuiProg <javierpatrick79@gmail.com>
2024-11-07 18:35:48 +0800

    ayan na boss sa side quest
    side quest boss

--- Assets/Desert_BOSS.cs
+++ Assets/Desert_BOSS.cs
@@ -6,22 +6,53 @@ using UnityEngine;
 public class Desert_BOSS : Enemy
 {
     bool attacking = false;
-    bool spottedPlayer = false;
+    public bool spottedPlayer = false;
     public float chaseDistance;
+    bool MusicPlaying = false;
     Animator anim;
+    [SerializeField] GameObject HEALTHBAR;
+    [SerializeField] GameObject BORDER_R;
+    [SerializeField] GameObject BORDER_L;
+    [SerializeField] AudioSource music;
+    bool dead = false;
+    public static Desert_BOSS instance;
+    
     protected override void Start()
     {
         base.Start();
         canAttack = true;
         canMove = true;
         anim = GetComponent<Animator>();
+        rb.gravityScale = 12f;
         ChangeStates(EnemyStates.DB_Idle);
+        BORDER_L.SetActive(false);
+        BORDER_R.SetActive(false);
+        HEALTHBAR.SetActive(false);
+    }
+    private void Awake()
+    {
+        HEALTHBAR.SetActive(PlayerPrefs.GetInt("Desert") != 1);
+        gameObject.SetActive(PlayerPrefs.GetInt("Desert") != 1);
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
     }
 
+   
     protected override void UpdateEnemyStates()
     {
         float _dist = Vector2.Distance(PlayerController.Instance.transform.position, transform.position);
         statusCheck();
+        if (health <= 0 && !dead)
+        {
+            PlayerController.Instance.levels = PlayerController.Instance.levels + 1;
+            dead = true;
+        }
         if (canMove && !attacking)
         {
             switch (currentEnemyStates)
@@ -29,6 +60,7 @@ public class Desert_BOSS : Enemy
                 case EnemyStates.DB_Idle:
                     if (_dist < chaseDistance)
                     {
+                        
                         spottedPlayer = true;
                         ChangeStates(EnemyStates.DB_Chase);
                     }
@@ -49,6 +81,29 @@ public class Desert_BOSS : Enemy
     {
         canMove = !parried;
         if (!attacking) flip();
+        HEALTHBAR.SetActive(spottedPlayer && PlayerPrefs.GetInt("Desert") != 1);
+        BORDER_L.SetActive(spottedPlayer && PlayerPrefs.GetInt("Desert") != 1);
+        BORDER_R.SetActive(spottedPlayer && PlayerPrefs.GetInt("Desert") != 1);
+        
+        if (health <= 0)
+        {
+            canMove = false;
+            canAttack = false;
+            music.volume -= Time.deltaTime;
+            spottedPlayer = false;
+            HEALTHBAR.SetActive(false);
+            BORDER_L.SetActive(false);
+            BORDER_R.SetActive(false);
+            PlayerPrefs.SetInt("Desert", 1);
+            Destroy(gameObject, 2f);
+        }
+
+        if (!MusicPlaying && spottedPlayer)
+        {
+            MusicPlaying = true;
+            music.Play();
+        }
+        
     }
     bool inRange()
     {
@@ -72,7 +127,7 @@ public class Desert_BOSS : Enemy
     }
     void attackBehavior()
     {
-        int attacks = Random.Range(0,4);
+        int attacks = Random.Range(0,5);
         if (!attacking)
         {
             switch (attacks)
@@ -88,6 +143,9 @@ public class Desert_BOSS : Enemy
                     break;
                 case 3:
                     StartCoroutine(Attack4());
+                    break;
+                case 4:
+                    StartCoroutine(Attack5());
                     break;
                 default:
                     break;
@@ -110,6 +168,16 @@ public class Desert_BOSS : Enemy
 
     //ATTACKS WALA PHASE 2
 
+    IEnumerator Attack5()
+    {
+        canMove = false;
+        attacking = true;
+        anim.SetTrigger("AirAttack");
+        yield return new WaitForSeconds(1f);
+        canMove = true;
+        attacking = false;
+        ChangeStates(EnemyStates.DB_Idle);
+    }
     IEnumerator Attack1()
     {
         canMove = false;
